8b541a6755401b972e069ca6b6c2961b62f71005
kwarzik <dmitriev89076@gmail.com>
2025-10-31 11:38:34 +0200

    Update Program.cs

--- Вычисление бесконечных сумм 10 вариант/Program.cs
+++ Вычисление бесконечных сумм 10 вариант/Program.cs
@@ -4,13 +4,11 @@
     int n = int.Parse(Console.ReadLine());
     Console.WriteLine("Введите значение x: ");
     double x =int.Parse(Console.ReadLine());
-    Console.WriteLine("Введите факториал с:");
-    double c =int.Parse(Console.ReadLine());
     double s = 0;
     for (int i = 0; i <= n; i++)
     {
-        с=(с-1);
-        double F = c;
+        long f = 1;
+        for (int j = 1; j <= i; j++) f *= j;
         s += Math.Cos(i * x) / F;
     }
     Console.WriteLine($"S={s:F2}");
